1efa892aaee768ef791bf8713f8426a688873401
Toaster <ttoasterrr@gmail.com>
2021-07-15 07:40:31 -0600

    Added >>mtg and >>wolfram

--- bot/cogs/games.py
+++ bot/cogs/games.py
@@ -1,7 +1,9 @@
 import ovalia_auxiliary_protocol as oap
 from discord.ext import commands
 from importlib import reload
+import requests
 import discord
+import urllib
 
 
 class Games(commands.Cog, description="A lotta commands for a lotta games"):
@@ -28,7 +30,7 @@ class Games(commands.Cog, description="A lotta commands for a lotta games"):
         if server_data.get("delete_invocation") == True:
             await oap.tryDelete(ctx)
     
-        acnh_data = oap.getJson("acnh_data")
+        acnh_data = oap.getJson("./cogs/acnh_data")
 
         if type == "" or type not in ["villager", "insect", "fish"]:
             return await oap.give_error(
@@ -159,6 +161,183 @@ class Games(commands.Cog, description="A lotta commands for a lotta games"):
                 cog=self.cog_name,
                 ctx=ctx
             )
+
+
+    # ==================================================
+    # 
+    # ==================================================
+    @commands.command(brief="", help="""\
+        help
+        """)
+    async def mtg(self, ctx, category="", value="", order="released", which="first"):
+        server_data = oap.getJson(f"servers/{ctx.guild.id}")
+        if server_data.get("delete_invocation") == True:
+            await oap.tryDelete(ctx)
+
+        valid_categories = [
+            "card"
+        ]
+
+        if category not in valid_categories:
+            return await oap.give_error(
+                text = f"Please give a valid category!",
+                categories = valid_categories,
+                category_title = "Categories",
+                examples = [
+                    ">>mtg card \"Island\""
+                ],
+                ctx = ctx
+            )
+
+        if value == "":
+            return await oap.give_error(
+                text = f"Please give a value to search!",
+                examples = [
+                    ">>mtg card \"Island\""
+                ],
+                ctx = ctx
+            )
+
+        valid_orders = [
+            "name",
+            "set",
+            "released",
+            "rarity",
+            "color",
+            "usd",
+            "tix",
+            "eur",
+            "cmc",
+            "power",
+            "toughness",
+            "edhrec",
+            "artist"
+        ]
+
+        if order not in valid_orders:
+            return await oap.give_error(
+                text = f"Please give a valid order!",
+                categories = valid_orders,
+                category_title = "Orders",
+                ctx = ctx
+            )
+
+        valid_which = [
+            "first",
+            "last"
+        ]
+
+        if which not in valid_which:
+            return await oap.give_error(
+                text = f"Please give a valid card to get!",
+                categories = valid_which,
+                category_title = "Cards",
+                ctx = ctx
+            )
+
+        response = requests.get(f"https://api.scryfall.com/{category}s/search?q={urllib.parse.quote(value)}&order={order}&include_extras=true&include_variations=true")
+
+        if which == "first":
+            card = response.json()['data'][0]
+        else:
+            card = response.json()['data'][-1]
+
+        embed = oap.makeEmbed(
+            title = f"{card['name']}",
+            description = f"{card['rarity']}",
+            image = f"{card['image_uris']['normal']}",
+            ctx = ctx
+        )
+
+        embed.add_field(
+            name = "Price",
+            value = f"{card['prices']['usd']}"
+        )
+
+        embed.add_field(
+            name = "Foil Price",
+            value = f"{card['prices']['usd_foil']}"
+        )
+
+        embed.add_field(
+            name = "Set",
+            value = f"{card['set_name']}",
+            inline = False
+        )
+
+        embed.add_field(
+            name = "Type",
+            value = f"{card['type_line']}",
+            inline = False
+        )
+
+        embed.add_field(
+            name = "Effect",
+            value = f"{card['oracle_text']}",
+            inline = False
+        )
+
+        embed.add_field(
+            name = "Total Cards Found",
+            value = f"{response.json()['total_cards']}",
+            inline = False
+        )
+
+        embed.add_field(
+            name = "Cards Found",
+            value = "\n".join([response.json()['data'][i]['name'] for i in range(response.json()['total_cards'] if response.json()['total_cards'] <= 10 else 10)]) + ("\n..." if response.json()['total_cards'] > 10 else ""),
+            inline = False
+        )
+
+        embed.set_footer(text=f"https://api.scryfall.com/{category}s/search?q={urllib.parse.quote(value)}&order={order}")
+
+        return await oap.give_output(
+            embed = embed,
+            log_text = f"Got mtg data",
+            cog = self.cog_name,
+            ctx = ctx,
+            data = server_data
+        )
+
+
+    # ==================================================
+    # 
+    # ==================================================
+    @commands.command(brief="", help="""\
+        help
+        """)
+    async def wolfram(self, ctx, *, input=""):
+        server_data = oap.getJson(f"servers/{ctx.guild.id}")
+        if server_data.get("delete_invocation") == True:
+            await oap.tryDelete(ctx)
+
+        if input == "":
+            return await oap.give_error(
+                text = f"Please give input for wolfram alpha!",
+                examples = [
+                    ">>wolfram 2x2",
+                    ">>wolfram roll 1d20",
+                    ">>wolfram what is the parity of sin(x)"
+                ],
+                ctx = ctx
+            )
+
+        response = requests.get(f"http://api.wolframalpha.com/v1/simple?appid=A4HU5T-9PRV4Q9L4T&i={urllib.parse.quote(input)}")
+
+        embed = oap.makeEmbed(
+            title = f"{input}",
+            description = f"{response}",
+            image = f"{response.url}",
+            ctx = ctx
+        )
+
+        return await oap.give_output(
+            embed = embed,
+            log_text = f"Asked wolfram alpha a question",
+            cog = self.cog_name,
+            ctx = ctx
+        )
+
 
 
 def setup(bot):
